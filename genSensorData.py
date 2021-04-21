24924fbe36a6a02c9db78558b554d335ebe7b7bb
apelerin <pelerin.axel@gmail.com>
2021-04-21 15:03:50 +0200

    Story 4: Transforms OK and ERROR from genTick to data lines.

--- genSensorData.py
+++ genSensorData.py
@@ -1,5 +1,17 @@
 import fileinput
 import sys
+import random
+
+error_messages = ["Too many conspiracy per mom in your house, please check you mom sanity.",
+        "Less than one conspiracy per mom, please check your mom pulse.",
+        "Weird number of conspiracy per mom, please check the number of mom."]
 
 for line in fileinput.input():
-    sys.stdout.write(line)
+    print(line)
+    if (line == "OK\n"):
+        sensor_nbr = str(random.randint(1, 100))
+        value = str(random.randint(1, 69))
+        print("Sensor#" + sensor_nbr + ";LivingRoom;value=" + value + ";unit=conspiracy/mom;")
+    elif (line == "ERROR\n"):
+        error_number = str(random.randint(1, 69))
+        print("Error#" + error_number + ";" + random.choice(error_messages) + ";")
