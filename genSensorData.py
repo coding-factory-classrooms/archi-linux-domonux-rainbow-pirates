b0246768788f45cb05e0f9f6e7e403a76ecd3698
apelerin <pelerin.axel@gmail.com>
2021-04-22 17:13:17 +0200

    S6: Filter stdin datas

--- genSensorData.py
+++ genSensorData.py
@@ -6,11 +6,14 @@ error_messages = ["Too many conspiracy per mom in your house, please check you m
         "Less than one conspiracy per mom, please check your mom pulse.",
         "Weird number of conspiracy per mom, please check the number of mom."]
 
+max_value = 0
 for line in fileinput.input():
     if (line == "OK\n"):
         sensor_nbr = str(random.randint(1, 100))
-        value = str(random.randint(1, 69))
-        sys.stdout.write("Sensor#" + sensor_nbr + ";LivingRoom;value=" + value + ";unit=conspiracy/mom;\n")
+        value = random.randint(1, 69)
+        if (value > max_value):
+            max_value = value
+        sys.stdout.write("SensorID#" + sensor_nbr + ";SensorName=LivingRoom;Value=" + str(value) + ";MaxValue=" + str(max_value) + ";unit=conspiracy/mom;\n")
         sys.stdout.flush()
     elif (line == "ERROR\n"):
         error_number = str(random.randint(1, 69))
--- generation.sh
+++ generation.sh
@@ -2,15 +2,25 @@
 gcc genTick.c -o genTick;
 
 destdir=./logs/$2
-
-if [ -f "$destdir" ]
-then
-    echo "Dir exist"
-fi
+regex="Sensor|Value"
 
 ./genTick $1 | python3 genSensorData.py | {
     while IFS= read -r line; do
-        echo $line >> $destdir/$3
+        
+        # Use separator ';' to store in array values differents datas for parsing
+        OLDIFS=$IFS
+        IFS=';'
+        read -a values <<< "$line"
+        IFS=$OLDIFS
+        str_to_store=""
+        for val in ${values[@]};
+        do
+            if [[ $val =~ $regex ]]
+            then
+                str_to_store+="$val;"
+            fi
+        done
+        echo $str_to_store >> $destdir/$3
     done
 }
 
