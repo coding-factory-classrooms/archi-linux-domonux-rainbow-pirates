44672d3276024508330983c4db154afcb90afb15
anis bastide <anis.bastide@gmail.com>
2021-04-22 14:33:37 +0200

    story 8 part 2

--- supervision.sh
+++ supervision.sh
@@ -3,11 +3,23 @@
 # $2: a file name
 # $3: a file name
 # $4: a file size (in bytes)
-for file in $(ls | grep \.log);do
-    size=$(stat -c%s "$file");
-    echo $size;
-    if [ $size -gt $4 ]
-    then
-    echo "too big";
-    fi
+
+if ! $(pgrep -x generation.sh);then
+    echo "not launched";
+    exit
+fi
+if ! [[! -f $1] then];then
+    echo "directory does not exist"
+    exit
+fi
+pid=$(pgrep -u $USER generation.sh )
+while $(pgrep -x generation.sh);do
+    for file in $(ls | grep \.log);do
+        size=$(stat -c%s "$file");
+        echo $size;
+        if [ $size -gt $4 ] then
+            kill $pid;
+            echo "too big";
+        fi
+    done
 done
